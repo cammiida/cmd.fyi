8fcec0738db4a4be4d702ae387a0d785e451079a
Camilla <camillamdalan@gmail.com>
2022-06-12 20:19:41 +0200

    :technologist::sparkles: Fix dev script and update content on home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,30 +1,24 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
+      <h1>Welcome to cmd.fyi</h1>
+      <p>This is a personal homepage for testing out new ideas and tech.</p>
+      <p>
+        Following is a list of ideas I would like to add to the site or
+        technologies I want to test out:
+      </p>
+      <ul style={{ listStyle: "none" }}>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <input type="checkbox" /> Set up a nice CI/CD pipeline with Docker and
+          hosted in AWS.
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
+          <input type="checkbox" /> Create a TIL page with login (admin page)
+          and a db. Go simple with Firebase first?
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <input type="checkbox" /> Create a library of books I have read or
+          want to read and my thoughts about them.
         </li>
       </ul>
     </div>
