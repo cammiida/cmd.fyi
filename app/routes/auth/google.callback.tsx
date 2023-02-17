6ba9b8d716d5e63b99fc4296939f4db5cf4f2364
Camilla <camillamdalan@gmail.com>
2023-02-17 23:40:16 +0100

    :bug: fix auth failure redirect bug

--- app/routes/auth/google.callback.tsx
+++ app/routes/auth/google.callback.tsx
@@ -4,6 +4,6 @@ import { authenticator } from "~/services/auth.server";
 export let loader: LoaderFunction = ({ request }) => {
   return authenticator.authenticate("google", request, {
     successRedirect: "/",
-    failureRedirect: "/login",
+    failureRedirect: "/",
   });
 };
