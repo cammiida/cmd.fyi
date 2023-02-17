2ae8b9e861ebaad7053d598338a3292eb19154d1
Camilla <camillamdalan@gmail.com>
2023-02-18 00:39:18 +0100

    :bug: fix callbackurl

--- app/services/google-strategy.ts
+++ app/services/google-strategy.ts
@@ -4,9 +4,9 @@ export const googleStrategy = new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID ?? "",
     clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    callbackURL:
-      process.env.GOOGLE_CALLBACK_URL ??
-      "http://localhost:3000/auth/google/callback",
+    callbackURL: `${
+      process.env.APP_HOST_NAME ?? "http://localhost:3000"
+    }/auth/google/callback`,
     scope: ["openid", "profile", "email"],
   },
   async ({ accessToken, refreshToken, extraParams, profile }) => {
