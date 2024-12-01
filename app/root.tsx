76479e453235cfe6b04b52ae10650846a8378897
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-12-01 17:09:41 +0100

    sets v2 flags for errorBoundary, normalizeFormMethod, meta and headers to true

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
   Links,
@@ -14,11 +14,11 @@ import styles from "./styles/app.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "cmd.fyi",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const meta: V2_MetaFunction = () => [
+  { charSet: "utf-8" },
+  { title: "cmd.fyi" },
+  { viewport: "width=device-width,initial-scale=1" },
+];
 
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await authenticator.isAuthenticated(request);
--- remix.config.js
+++ remix.config.js
@@ -17,5 +17,9 @@ module.exports = {
   // publicPath: "/build/",
   future: {
     v2_dev: true,
+    v2_errorBoundary: true,
+    v2_normalizeFormMethod: true,
+    v2_meta: true,
+    v2_headers: true,
   },
 };
