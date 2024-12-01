353dfd24e309b29b89d804865bcca9ac3691e3bd
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-12-01 17:22:59 +0100

    upgrades to remix v2

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
   Links,
@@ -14,7 +14,7 @@ import styles from "./styles/app.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
-export const meta: V2_MetaFunction = () => [
+export const meta: MetaFunction = () => [
   { charSet: "utf-8" },
   { title: "cmd.fyi" },
   { viewport: "width=device-width,initial-scale=1" },
