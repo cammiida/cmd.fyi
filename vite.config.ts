5aa466fdaabb74036b460d16bf4c6cb3ec6d0534
Camilla Marie Dalan <camillamdalan@gmail.com>
2024-12-15 20:52:55 +0100

    removes server port in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 installGlobals();
 
 export default defineConfig({
-  server: { port: 3000 },
   plugins: [
     remix({ ignoredRouteFiles: ["**/.*"] }),
     netlifyPlugin(),
